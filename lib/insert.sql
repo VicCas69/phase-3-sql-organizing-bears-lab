405d5fd30bcb531730eef3bb5dd164caef297ec5
victor castro <victor_castro_01@hotmail.com>
2022-04-28 17:58:22 -0400

    commit tree edbeffb1f8fa16a97198a2ada8c0b94b2bc59776

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "blue", "happy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 10, "M", "yellow", "sad", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "F", "red", "social", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 30, "M", "green", "strict", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 13, "F", "pink", "extrovert", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 2, "M", "black", "outgoing", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 6, "F", "brown", "shy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 40, "M", "white", "angry", 0);
